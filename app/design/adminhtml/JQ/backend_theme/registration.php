5db7222996610d87f1f6ba9c3d2969c32f83b39f
jq <jasieq91@gmail.com>
2022-03-14 20:41:04 +0100

    Initial commit

---
+++ app/design/adminhtml/JQ/backend_theme/registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Copyright (c) 2022.
+ * Author	JQ
+ * Category	backend_theme
+ *
+ *
+ *
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'adminhtml/JQ/backend_theme', // Example: 'adminhtml/Magento/backend'
+    __DIR__
+);
